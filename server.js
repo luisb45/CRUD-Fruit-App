2b3c6c23577ae7c5b04a46847349e5a602a4bf0d
Luis Bustos <luisbustos500@gmail.com>
2022-11-15 15:35:53 -0700

    redirect user to another page

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 //require
 const express = require('express');
 const app = express();
+const fruits = require('./models/fruits');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 // ========== Connection to database =============== //
 
 
 
 // =========== MiddleWare ============== //
-const fruits = ['apple', 'banana', 'pear'];
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+app.use(express.urlencoded({extended:false}));
+
 
 
 // ======= Routes ======= //
@@ -16,19 +24,37 @@ const fruits = ['apple', 'banana', 'pear'];
 
 //Index
 app.get('/fruits', (req, res) => {
-    res.send(fruits);
+    res.render('Index', {fruits: fruits });
 });
-//New
 
+//New
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+})
 //Delete
 
 //Update
 
 //Create
-
+app.post('/fruits', (req, res) => {
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.redirect('/fruits');
+});
 //Edit
 
 //Show
+app.get('/fruits/:index', (req, res) => {
+    res.render('Show', {
+        fruit: fruits[req.params.index]
+    });
+})
+
 app.listen(3000, () => {
     console.log('listening');
 });
